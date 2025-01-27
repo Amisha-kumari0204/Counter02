49a5492047da727fcbba2c1cce40e9c42e38e502
Amisha Kumari <137042093+Amisha-kumari0204@users.noreply.github.com>
2025-01-28 00:33:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+function App() {
+  let[counter,setcounter]=useState(14);
+ 
+  const addvalue=()=>{
+    console.log("value added",counter);
+    if(counter==20)
+      counter=19;
+    setcounter(counter+1);
+
+  }
+ const removvalue=()=>{
+  console.log("value removed",counter);
+  if(counter<1)
+    counter=0;
+  
+  setcounter(counter-1);
+  
+    
+ }
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter value:{counter}</h2>
+    <button onClick={addvalue}>Add value</button>
+    <br/>
+    <button onClick={removvalue}>remove value</button>
+     
+
+        
+    </>
+  
+  );
+}
+
+export default App;
